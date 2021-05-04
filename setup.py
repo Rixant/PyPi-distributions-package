81544886bcfe4a1a2ddc576e6b25c3fec80fe2af
rixant <rishantrokaha@gmail.com>
2021-05-04 13:23:11 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='math-binomial-gaussian-distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['math-binomial-gaussian-distribution'],
+      author='Rishant Rokaha',
+      
+      zip_safe=False)
